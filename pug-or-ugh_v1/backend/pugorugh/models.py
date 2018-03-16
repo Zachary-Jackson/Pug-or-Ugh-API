2a186efe5c88db2e1c309611b17f027d816f3715
Zachary-Jackson <jackson.zachary.r@gmail.com>
2018-03-16 16:22:19 -0400

    Added Models and Updated requirments
    The Dog, UserDog, and UserPref models have been added. The requirements.txt file has been added to include Pillow

--- pug-or-ugh_v1/backend/pugorugh/models.py
+++ pug-or-ugh_v1/backend/pugorugh/models.py
@@ -2,3 +2,26 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Dog(models.Model):
+    """This is the model for a dog"""
+    name = models.CharField(max_length=100)
+    image_filename = models.ImageField()
+    breed = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    size = models.CharField(max_length=2)
+
+
+class UserDog(models.Model):
+    """This is the class that links dogs with Users"""
+    user = models.OneToOneField(User)
+    dog = models.ManyToManyField(Dog)
+    status = models.CharField(max_length=1)
+
+
+class UserPref(models.Model):
+    """This class sets a User's preference about a dog"""
+    user = models.OneToOneField(User)
+    age = models.CharField(max_length=7)
+    gender = models.CharField(max_length=3)
+    size = models.CharField(max_length=8)
